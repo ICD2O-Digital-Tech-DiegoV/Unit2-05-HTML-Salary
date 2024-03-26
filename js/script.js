d20a2c5107840726ba23dfaf3ec44135bd78d913
DiegoValdivieso22 <159164145+DiegoValdivieso22@users.noreply.github.com>
2024-03-26 14:06:21 +0000

    cote edit

--- js/script.js
+++ js/script.js
@@ -3,3 +3,22 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict";
+/**
+ * This function calculates salary.
+ */
+function calculate() {
+  // input
+  let hoursWorked = parseInt(document.getElementById('hoursWorked').value)
+  let hourlyRate = parseInt(document.getElementById('hourlyRate').value)
+
+  // process
+  let moneyEarned = hoursWorked * hourlyRate
+  let govTax = moneyEarned * 0.18
+
+
+  // output
+  document.getElementById('preTaxTotal').innerHTML = ': Hours Worked ' + 'Hourly Rate'
+  document.getElementById('afterTaxTotal').innerHTML += '<br>' + hoursWorked + ' * ' + hour
+
+}
